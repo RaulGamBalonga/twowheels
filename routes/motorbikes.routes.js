bb63ecb6ab10db3b5348c1aaafb1c318a7047e3a
RaulGamBalonga <88031305+RaulGamBalonga@users.noreply.github.com>
2021-11-23 18:59:07 +0100

    crud motorbikes done

--- routes/motorbikes.routes.js
+++ routes/motorbikes.routes.js
@@ -4,14 +4,15 @@ const Motorbike = require("../models/Motorbike.model");
 const router = require("express").Router();
 
 // Endpoints
-/* CREAR -create[<C>-R-U-D] */
+/* CREAR -create[<C>-R-U-D] Crear modelo de moto con su tipo su marca su imagen etc.. */
+
 router.get("/new", (req, res) => {
-    
+
     res.render("motorbikes/create-motorbikes", {
-        
+
         brands: ["BMW", "HONDA", "HARLEY-DAVIDSON"],
         typesMotorbike: ["URBAN", "TRAIL", "CUSTOM"],
-        license: ["A1", "A2", "A"] 
+        license: ["A1", "A2", "A"]
     })
 
 })
@@ -22,18 +23,85 @@ router.post("/new", (req, res) => {
 
 
     Motorbike.create({ name, typesMotorbike, description, brand, cc, weight, imageURL, license, })
-        .then(createdMotorbike => res.redirect("/"))
+        .then(createdMotorbike => res.redirect("/motorbikes/new"))
         .catch(err => console.log(err))
 }
 )
 
 /* LEER -read [C-<R>-U-D] */
-// listado de todas las montañas rusas
-router.get("/", (req, res, next) => {
+// listado de todas las motos
+router.get("/list", (req, res, next) => {
 
     Motorbike.find()
 
-        .then(motorbike => res.render("motorbikes/read-motorbike", { motorbike }))
+        .then(motorbikes => res.render("motorbikes/list-motorbikes", { motorbikes }))
         .catch(err => console.log(err))
 });
+
+/* BORRAR -delete [C-R-U-<D>] */
+// Borrar una moto
+
+router.get("/delete", (req, res) => {
+    const { id } = req.query
+
+
+    Motorbike.findByIdAndDelete(id)
+        .then(info => {
+            console.log(info)
+            res.redirect("/motorbikes/list")
+        })
+        .catch(err => console.log(err))
+
+})
+
+// /* EDITAR -update [C-R-<U>-D] */
+//  editar modelo moto
+router.get("/edit/:id", (req, res) => {
+    const { id } = req.params
+    Motorbike.findById(id)
+        .then((motorbike) => {
+            res.render("motorbikes/update-motorbikes", {
+                motorbike,
+                brands: ["BMW", "HONDA", "HARLEY-DAVIDSON"],
+                typesMotorbike: ["URBAN", "TRAIL", "CUSTOM"],
+                license: ["A1", "A2", "A"]
+            })
+        })
+        .catch(err => console.log(err))
+
+
+
+
+})
+
+
+
+
+router.post("/edit", (req, res) => {
+    const { id } = req.query
+    const { name, typesMotorbike, description, brand, cc, weight, imageURL, license } = req.body
+
+    Motorbike.findByIdAndUpdate(id, { name, typesMotorbike, description, brand, cc, weight, imageURL, license }, { new: true })
+        .then(updatedMotorbike => {
+            res.redirect("/motorbikes/list")
+        })
+        .catch(err => console.log(err))
+
+
+})
+
+/* LEER -read [C-<R>-U-D] */
+//Detallles de una moto
+router.get("/:id", (req, res, next) => {
+    const { id } = req.params
+
+    Motorbike.findById(id)
+
+        .then(motorbike => res.render("motorbikes/details-motorbikes", motorbike))
+        .catch(err => console.log(err))
+});
+
+
+
+
 module.exports = router
