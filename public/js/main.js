f7aac62f1b7897e36b513defbe315768b27b27c5
RaulGamBalonga <88031305+RaulGamBalonga@users.noreply.github.com>
2021-11-25 17:50:58 +0100

    segundo intento

--- public/js/main.js
+++ public/js/main.js
@@ -18,18 +18,54 @@ window.onload = () => {
         lng: undefined
     };
 
+    getItineraries(map)
+        .then(itineraries => {
+            // 7. Instrucciones: Llamar a placeRestaurants pasandoles la info
+            const markers = placeItineraries(map, itineraries)
+        })
+        .catch(error => console.log(error))
+
 
     // public/javascripts/main.js
 
-    function getRestaurants() {
-        axios.get("/restaurants/api")
+    function getItineraries() {
+        axios.get("/itineraries/api")
             .then(response => {
-                placeRestaurants(response.data.restaurants);
+                placeItineraries(response.data.itineraries);
             })
             .catch(error => {
                 console.log(error);
             })
     }
+
+    // 6. Instrucciones: Creado y llamado a getRestaurants para recuperar
+    //    esa info de la BD
+    function getItineraries() {
+        return axios.get("/restaurants/api")
+            .then(response => response.data.itineraries)
+    }
+
+
+
+    function placeRestaurants(map, restaurants) {
+        const markers = []
+
+        // 8. Instrucciones: Por cada restaurante creo un nuevo Marker
+        restaurants.forEach((restaurant) => {
+            const center = {
+                lat: restaurant.location.coordinates[1],
+                lng: restaurant.location.coordinates[0]
+            };
+            const newMarker = new google.maps.Marker({
+                position: center,
+                map: map,
+                title: restaurant.name
+            });
+            markers.push(newMarker);
+        });
+
+        // 9. Instrucciones: Finalmente retorno los markers por si los necesitase a futuro
+        return markers
 
 };
 
